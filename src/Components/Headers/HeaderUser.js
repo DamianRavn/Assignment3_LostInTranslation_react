c222181ff28bbecc908a0099e350a544b6a612bb
Kasper Gade Andreasen <kasper.andreasen@gmail.com>
2022-01-31 15:04:13 +0100

    Feat: Added 3 new Components. One to view and two to components. Creating separate headers depending on state. Not fully implemented yet.

--- src/Components/Headers/HeaderUser.js
+++ src/Components/Headers/HeaderUser.js
@@ -1,6 +1,6 @@
-import { Nav, NavDropdown, Container, Navbar } from 'react-bootstrap';
+import { Nav, Container, Navbar } from 'react-bootstrap';
 
-function Header() {
+function HeaderUser() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -17,4 +17,4 @@ function Header() {
     );
 }
 
-export default Header;
+export default HeaderUser;
---
+++ src/views/Header.js
@@ -0,0 +1,22 @@
+import HeaderUser from "../Components/Headers/HeaderUser";
+
+function Header() {
+    // Add conditional rendering here:
+    /** if (user is in state) {
+     * // Render HeaderUser
+     *  return (
+     *  <HeaderUser />
+     * )
+     * } else {
+     *      Render HeaderGuest
+     *   return (
+     *  <HeaderGuest />
+     * )
+     * }
+     *  */
+    return (
+        <HeaderUser />
+    )
+}
+
+export default Header;
