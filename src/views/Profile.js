fc569ebd0ee2380911a274450f12a273e995cfc7
Kasper Gade Andreasen <kasper.andreasen@gmail.com>
2022-02-01 22:42:18 +0100

    KGA feat: Got the local session to work. It now registers and forces you to stay on either profile or translation page. Also the logout button correctly empties localstorage and send you to the landingpage.

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,23 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { logout } from "../features/user.js";
 
 function Profile() {
     const user = useSelector((state) => state.user.value);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1>Profile</h1>
-            <h5>User: {user.username}</h5>
-            <p>Translations: {user.translations}</p>
-
+        <div className="container text-center d-flex align-items-center justify-content-center">
             <div>
-                <button onClick=
-                    {
-                        () => {
-                            dispatch(logout());
+                <div>
+                    <h1>Profile</h1>
+                </div>
+                <div>
+                    <h5>User: {user.username}</h5>
+                </div>
+                <div>
+                    <p>Translations: {user.translations}</p>
+                </div>
+                <div>
+                    <button onClick=
+                        {
+                            () => {
+                                dispatch(logout());
+                                navigate("/");
+                            }
                         }
-                    }
-                >Logout</button>
+                    >Logout</button>
+                </div>
             </div>
         </div>
     );
