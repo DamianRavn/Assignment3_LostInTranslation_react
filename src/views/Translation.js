7f44d603fe8c4303876c37d7591d5f163ef087db
Kasper Gade Andreasen <kasper.andreasen@gmail.com>
2022-02-01 13:40:10 +0100

    KGA fix: Removed the two files I was semi working on. Sneaking suspecion that someone  else is working there, so to avoid conflicts.

--- src/views/Translation.js
+++ src/views/Translation.js
@@ -1,11 +1,9 @@
 import { React } from "react";
-import TranslationOutput from "../Components/Translation/TranslationOutput";
 
 function Translation(props)
 {
     return(
         <div>
-            <TranslationOutput />
         </div>
     );
 }
