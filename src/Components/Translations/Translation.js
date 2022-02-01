e223f396414ea63cfd223ea50339d62c54d77220
Kasper Gade Andreasen <kasper.andreasen@gmail.com>
2022-02-01 18:53:06 +0100

    KGA feat: Got the rendering to work, but not properly. It's pretty hacky as it stands, but I might be able to fix that.

--- src/Components/Translations/Translation.js
+++ src/Components/Translations/Translation.js
@@ -4,67 +4,71 @@ import { useDispatch } from 'react-redux';
 
 const Translation = (props) => {
     const [translation, setTranslation] = useState("");
+    const [translatedSentence, setTrancelatedSentence] = useState([]);
     const dispatch = useDispatch();
-    const imageSource = "src/assets/images/signs/";
-    let translatedSentence = [];
+    const imageSource = "assets/images/signs/";
+    //const translatedSentence = [];
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(translation);
-        handleTranslation(translation)
+        handleTranslation(translation);
     }
 
     const handleTranslation = (sentence) => {
         let translatedLetter = '';
-        translatedSentence = [];
         sentence.toString().toLowerCase();
-        for(let index = 0; index < sentence.length; index++) {
-            if(/([a-z])/g) {
+        for (let index = 0; index < sentence.length; index++) {
+            if (/([a-z])/g) {
                 translatedLetter = sentence.charAt(index) + '.png';
             }
-            translatedSentence.push(<img src={imageSource + translatedLetter} alt=" "></img>)
+
+            translatedSentence.push({ "id": index, "src": imageSource, "letter": translatedLetter });
+            //setTrancelatedSentence(translatedSentence => [...translatedSentence, imageSource + translatedLetter]);
+
+            //translatedSentence.push(imageSource + translatedLetter);
+            setTrancelatedSentence(translatedSentence);
             //translatedSentence.push(translatedLetter)
         }
-        
+
         console.log(translatedSentence);
-        //for(const [value] of translatedSentence.entries()) {
+        // for(const [value] of translatedSentence.entries()) {
         //    images.push(<img src={imageSource + value} alt=" "/>)
-        //}
+        // }
+        // console.log("[Images]", images);
+        // console.log("[Src's]", translatedSentence);
     }
-
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="translation">Sentence you would like to translate</label>
-            <input 
-                id="translation"
-                type="text"
-                placeholder="Enter sentence..."
-                className="form-control"
-                onChange={(event) => setTranslation(event.target.value)}
-            />
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="translation">Sentence you would like to translate</label>
+                <input
+                    id="translation"
+                    type="text"
+                    placeholder="Enter sentence..."
+                    className="form-control"
+                    onChange={(event) => setTranslation(event.target.value)}
+                />
 
-            <button 
-                type="submit" 
-                className="btn btn-success btn-lg" 
-                onClick={() =>
-                    handleSubmit(dispatch, translation)
-                }>
+                <button
+                    type="submit"
+                    className="btn btn-success btn-lg"
+                    /* onClick={() => handleSubmit(dispatch, translation)} */>
                     Translate
-            </button>
+                </button>
+            </form>
             <div>
-                {translatedSentence}
-                {/*
-                <ul>
-                    {translatedSentence.map((value, index) => {
-                        return <li key={index}>{value}</li>
-                    })}
-                </ul>
-                */}
+                {/* {translatedSentence.map((letter) => {
+                    return <p key={letter.id}>{letter.src + letter.letter}</p>;
+                    })} */}
+                {/* {translatedSentence[0].letter + translatedSentence[0].src} */}
+                {translatedSentence.map((letter) => {
+                    return <img className='handsigns' key={letter.id} src={letter.src + letter.letter} />
+                })}
             </div>
-        </form>
+        </>
     )
 
 }
 
 export default Translation;
-        
