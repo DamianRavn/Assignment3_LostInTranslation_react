276b895bbd81a7ab5ccaf048f122edbb8f94d8fb
Kasper Gade Andreasen <kasper.andreasen@gmail.com>
2022-02-01 22:46:16 +0100

    KGA fix: Found two typos that I overlooked when merging. Fixed!

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -7,36 +7,37 @@ function Profile() {
     const selectTranslations = createSelector(
         (state) => state.user,
         (translations) => translations.filter((translation) => !translation.deleted)
-      )
+    )
     const username = useSelector((state) => state.user.value.username);
     const translations = useSelector(selectTranslations);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     return (
         <div className="container text-center d-flex align-items-center justify-content-center">
-        <div>
-            <h1>Profile</h1>
-            <h5>User: {username}</h5>
-            <p>Translations: {translations}</p>
             <div>
+                <h1>Profile</h1>
+                <h5>User: {username}</h5>
+                <p>Translations: {translations}</p>
                 <div>
-                    <h1>Profile</h1>
-                </div>
-                <div>
-                    <h5>User: {user.username}</h5>
-                </div>
-                <div>
-                    <p>Translations: {user.translations}</p>
-                </div>
-                <div>
-                    <button onClick=
-                        {
-                            () => {
-                                dispatch(logout());
-                                navigate("/");
+                    <div>
+                        <h1>Profile</h1>
+                    </div>
+                    <div>
+                        <h5>User: {username}</h5>
+                    </div>
+                    <div>
+                        <p>Translations: {username.translations}</p>
+                    </div>
+                    <div>
+                        <button onClick=
+                            {
+                                () => {
+                                    dispatch(logout());
+                                    navigate("/");
+                                }
                             }
-                        }
-                    >Logout</button>
+                        >Logout</button>
+                    </div>
                 </div>
             </div>
         </div>
