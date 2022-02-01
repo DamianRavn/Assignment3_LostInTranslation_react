9ea019af0e753fced36310589d6b46405bb53b98
dragonalias <dragon1600@gmail.com>
2022-02-01 22:04:42 +0100

    feat: filtering translations WIP

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,14 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../features/user.js";
+import { createSelector } from 'reselect'
 
 function Profile() {
-    const user = useSelector((state) => state.user.value);
+    const selectTranslations = createSelector(
+        (state) => state.user,
+        (translations) => translations.filter((translation) => !translation.deleted)
+      )
+    const username = useSelector((state) => state.user.value.username);
+    const translations = useSelector(selectTranslations);
     const dispatch = useDispatch();
     return (
         <div>
             <h1>Profile</h1>
-            <h5>User: {user.username}</h5>
-            <p>Translations: {user.translations}</p>
+            <h5>User: {username}</h5>
+            <p>Translations: {translations}</p>
 
             <div>
                 <button onClick=
