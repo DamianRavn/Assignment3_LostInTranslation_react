d1217ef55b0d01e7f7984d8bed1508617ddb536b
Damian Ravn <DamianRavn@gmail.com>
2022-02-01 11:04:29 +0100

    feat: Updated the landingpage so it displays dynamically whether its loading or done

--- src/features/user.js
+++ src/features/user.js
@@ -3,13 +3,46 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 const apiURL = 'https://assignment3-react.herokuapp.com'
 const apiKey = "ffsgqnwrubathttxuatsbsgmkvqvflgeogojnxztvyllhqfhceqcfyznwtuzuyyv";
 
+//fetches user from api. Using redux toolkit thunk middleware
 export const fetchUser = createAsyncThunk('user/fetchUser',
-    async (username) => {
-        console.log(username);
+    async (username) => 
+    {
         return fetch(`${apiURL}/translations?username=${username}`).then((res) => res.json);
     });
 
+//creates user from api. Using redux toolkit thunk middleware
+export const createUser = createAsyncThunk('user/createUser',
+async (username) => 
+{
+    return fetch(`${apiURL}/translations`, 
+    {
+        method: 'POST',
+        headers: 
+        {
+          'X-API-Key': apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify
+        ({ 
+            username: username, 
+            translations: [] 
+        })
+    })
+    .then(response => 
+    {
+      if (!response.ok) 
+      {
+        throw new Error('Could not create new user')
+      }
+      return response.json()
+    })
+    .catch(error => 
+    {
+        console.log(error);
+    })
+});
 
+//base state
 const initialStateValue =
 {
     id: 0,
@@ -17,6 +50,7 @@ const initialStateValue =
     translations: [],
     status: ""
 };
+//Create slice is part of redux toolkit. It has the user state and reducers
 export const userSlice = createSlice
     ({
         name: "user",
@@ -37,19 +71,43 @@ export const userSlice = createSlice
         // Handles the async states
         extraReducers:
         {
-            [fetchUser.pending]: (state, action) => {
+            //Fetch user
+            [fetchUser.pending]: (state, action) => 
+            {
                 state.status = "loading";
                 console.log(state.status);
             },
-            [fetchUser.fulfilled]: (state, { payload }) => {
-                if (payload.length === 1) {
-                    console.log(payload);
-                    state.value = payload;
+            [fetchUser.fulfilled]: (state, payloadObject) => 
+            {
+                if (payloadObject.payload.length === 0) 
+                {
+                    console.log(payloadObject);
+                    state.value = payloadObject;
                 }
                 state.status = "sucess";
                 console.log(state.status);
             },
-            [fetchUser.rejected]: (state, action) => {
+            [fetchUser.rejected]: (state, action) => 
+            {
+                state.status = "failed";
+                state.error = action.error.message;
+                console.log(state.status);
+            },
+
+            //Create user
+            [createUser.pending]: (state, action) => 
+            {
+                state.status = "loading";
+                console.log(state.status);
+            },
+            [createUser.fulfilled]: (state, payloadObject) => 
+            {
+                console.log(payloadObject)
+                state.status = "sucess";
+                console.log(state.status);
+            },
+            [createUser.rejected]: (state, action) => 
+            {
                 state.status = "failed";
                 console.log(state.status);
             }
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,6 +1,7 @@
 import { React } from "react";
+import { useSelector } from "react-redux";
 import { fetchUser, setUserName } from "../features/user.js";
-import InputAsyncCallComponent from "../Components/InputAsyncCallComponent.js"
+import InputAsyncCallComponent from "../Components/InputAsyncCallComponent.js";
 
 const handleUserClick = (dispatch, username)=>
 {
@@ -10,18 +11,39 @@ const handleUserClick = (dispatch, username)=>
 
 function LandingPage(props)
 {
+    let content
+
+    const userStatus = useSelector(state => state.user.status);
+    const username = useSelector(state => state.user.name);
+    const error = useSelector(state => state.user.error);
+    console.log("user: " + userStatus + " username: " + username)
+    if (!username) 
+    {
+        content = <InputAsyncCallComponent clickHandler = {handleUserClick} placeholder = "Input Username here" />    
+    }
+    if (userStatus === 'loading') 
+    {
+        content = <div>Loading...</div>
+    } 
+    else if (userStatus === 'sucess') 
+    {
+        content = <div>Sucess!</div>
+    } 
+    else if (userStatus === 'failed') 
+    {
+      content = <div>{error}</div>
+    }
+    
     return(
         <>
         <div className="container text-center d-flex align-items-center justify-content-center">
             <div>
                 <h1 className="animate__animated animate__wobble">Welcome to the Translatron!</h1>
             </div> 
-        <div>
-            <div>Home</div>
-            <InputAsyncCallComponent clickHandler = {handleUserClick} placeholder = "Input Username here" />
-        </div>
-        <div className="containter text-center d-flex align-item-center justify-content-center mt-3">
-        <InputAsyncCallComponent clickHandler = {handleUserClick} />
+        
+            <div className="containter text-center d-flex align-item-center justify-content-center mt-3">
+            {content}
+            </div>
         </div>
         </>
     );
