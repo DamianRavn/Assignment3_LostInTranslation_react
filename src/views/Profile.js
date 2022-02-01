5b1322bf4aca27892d7d9c0836ccf5d180864c21
dragonalias <dragon1600@gmail.com>
2022-02-01 22:51:26 +0100

    feat: updating deleted translations WIP

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,29 +1,34 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../features/user.js";
-import { createSelector } from 'reselect'
 
 function Profile() {
-    const selectTranslations = createSelector(
-        (state) => state.user,
-        (translations) => translations.filter((translation) => !translation.deleted)
-      )
     const username = useSelector((state) => state.user.value.username);
-    const translations = useSelector(selectTranslations);
+
+    //Only show 10 translations, so we filter by deletion and count
+    const translations = useSelector((state) => state.user.value.translations);
+    const filteredTranslations = translations.filter((record, index)=> !record.deleted && index < 10);
+    //Cant display an obejct as easily as we can an array
+    const displayTranslations = filteredTranslations.map((translation)=> translation.translation + ' \n');
     const dispatch = useDispatch();
+
+    const handleClick = ()=>
+    {
+        dispatch()
+
+    }
     return (
         <div>
             <h1>Profile</h1>
             <h5>User: {username}</h5>
-            <p>Translations: {translations}</p>
+            <h5>Translations:</h5>
+            <pre>{displayTranslations}</pre>
 
             <div>
                 <button onClick=
                     {
-                        () => {
-                            dispatch(logout());
-                        }
+                        handleClick
                     }
-                >Logout</button>
+                >delete</button>
             </div>
         </div>
     );
