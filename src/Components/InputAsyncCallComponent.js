b53fdc2e564175000ddf319370c2c39580455bfa
Damian Ravn <DamianRavn@gmail.com>
2022-02-01 09:12:28 +0100

    docs: commented functions and fix: Made placeholder text be passed down from parent so that component can be resued easily

--- src/Components/InputAsyncCallComponent.js
+++ src/Components/InputAsyncCallComponent.js
@@ -4,11 +4,13 @@ import { useState } from "react";
 
 export default function InputAsyncCallComponent(props)
 {
+  //Making a local state so that the input is tracked
   const [name, setName] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = event => 
   {
+    //Prevents the refresh of the site
     event.preventDefault();
   };
 
@@ -17,11 +19,12 @@ export default function InputAsyncCallComponent(props)
       <input 
       type="text" 
       required={true}
-      placeholder="Input Username here"
+      placeholder= {props.placeholder}
       onChange={(e) => setName(e.target.value)}
       />
       <button onClick=
       {()=>
+        //When the button is clicked, run the function passed down from parent. This is an lambda function so it doesn't run on load.
         props.clickHandler(dispatch, name)
       }>Submit</button>
   </form>
