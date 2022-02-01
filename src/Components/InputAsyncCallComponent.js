81a6e9a4a8f61e5d1cc655ed4af2679e9e50a4da
Kasper Gade Andreasen <kasper.andreasen@gmail.com>
2022-02-01 09:00:33 +0100

    KGA Feat: Implemented conditional rendering of the headers based on userstate. Also added the images for the translation

--- src/Components/InputAsyncCallComponent.js
+++ src/Components/InputAsyncCallComponent.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 
-export default function InputAsyncCallComponent(props)
-{
+export default function InputAsyncCallComponent(props) {
   const [name, setName] = useState("");
   const dispatch = useDispatch();
 
-  const handleSubmit = event => 
-  {
+  const handleSubmit = event => {
     event.preventDefault();
   };
 
-  return (    
-  <form onSubmit={handleSubmit}>
-      <input 
-      type="text" 
-      required={true}
-      placeholder="Input Username here"
-      onChange={(e) => setName(e.target.value)}
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        required={true}
+        placeholder="Input Username here"
+        onChange={(e) => setName(e.target.value)}
       />
       <button onClick=
-      {()=>
-        props.clickHandler(dispatch, name)
-      }>Submit</button>
-  </form>
+        {() =>
+          props.clickHandler(dispatch, name)
+        }>Submit</button>
+    </form>
   );
 }
